7e341e7ce9e2d1b74b9a02380f4256fda31080dc
sandra <sandra@us-sandra-hoang.mynetworksettings.com>
2023-05-25 23:07:16 -0400

    Create about page and test commit

--- pages/about/chef.js
+++ pages/about/chef.js
@@ -1,14 +1,14 @@
 import Link from 'next/link';
 import Head from 'next/head';
-import Layout from '../components/layout'
+import Layout from '../../components/layout'
 
 export default function ProfilePage() {
     return (
       <Layout>
         <Head>
-          <title>First Post</title>
+          <title>About the Chef</title>
         </Head>
-        <h1>Profile Page</h1>
+        <h1>I'm a homemade chef honing in on my craft sharing my random recipes that I myself enjoy</h1>
         <h2>
           <Link href="/">Back to recipes</Link>
         </h2>
